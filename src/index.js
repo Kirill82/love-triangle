8a89b1a93faefbf7173c5b3dccf195e17531057b
Kirill Gorbikov <kirillhor@gmail.com>
2018-09-15 18:50:22 +0300

    loop for triangles

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var countTriangels = 0;
+  var corners = [];
+  for (let i = 0; i < preferences.length; i++) {
+    if (corners.indexOf(i) == -1) {
+      let cornerOne = preferences[i];
+      let cornerTwo = preferences[cornerOne - 1];
+      let cornerThree = preferences[cornerTwo - 1];
+      if ((cornerThree - 1) == i && cornerOne != cornerTwo) {
+        countTriangels += 1;
+        corners.push(i, cornerOne - 1, cornerTwo - 1);
+      }
+    }
+  }
+  return countTriangels;
 };
